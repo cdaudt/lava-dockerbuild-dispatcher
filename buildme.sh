5517c03a57b569a90557405bb0e392f7520b9bff
Christian Daudt <christian.daudt@cypress.com>
2017-02-21 13:18:36 -0600

    updated buildme

--- buildme.sh
+++ buildme.sh
@@ -1,25 +1,17 @@
 TAGNAME="lava/dispatcher"
-VER="cypress-1"
+VER="latest"
 ATTEMPT=0
-NOCACHE=${1:-false}
+NOCACHE=${1:-true}
 COPYTO=${2:-}
 echo "NOCACHE:${NOCACHE} COPYTO:${COPYTO}"
-while :
-do
-	docker build --no-cache=${NOCACHE} -t ${TAGNAME}:${VER} .
-	if [ $? -eq 0 ]
-	then 
-		break
-	fi
-	ATTEMPT=`expr $ATTEMPT + 1`
-	echo "Finished attempt ${ATTEMPT} unsuccessfully. Retrying"
-	date
-	echo =======================================================================================
-	sleep 10m
-done
+docker build --no-cache=${NOCACHE} -t ${TAGNAME}:${VER} .
+if [ $? -ne 0 ]
+then 
+  exit 1
+fi
 
 if [ "${COPYTO}A" != "A" ]
 then
-	docker tag ${TAGNAME}:${VER} ${COPYTO}/${TAGNAME}:${VER}
-	docker push ${COPYTO}/${TAGNAME}:${VER}
+  docker tag ${TAGNAME}:${VER} ${COPYTO}/${TAGNAME}:${VER}
+  docker push ${COPYTO}/${TAGNAME}:${VER}
 fi
