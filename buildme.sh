1530487cb5174831329e82972c44058a314de614
Christian Daudt <christian.daudt@cypress.com>
2017-03-07 14:02:04 -0600

    add metadata

--- buildme.sh
+++ buildme.sh
@@ -4,7 +4,12 @@ ATTEMPT=0
 NOCACHE=${1:-true}
 COPYTO=${2:-}
 echo "NOCACHE:${NOCACHE} COPYTO:${COPYTO}"
-docker build --no-cache=${NOCACHE} -t ${TAGNAME}:${VER} .
+docker build \
+  --label "build.source=`git log --oneline -1`" \
+  --label "build.status=`git status --short`" \
+  --no-cache=${NOCACHE} \
+  -t ${TAGNAME}:${VER} \
+  .
 if [ $? -ne 0 ]
 then 
   exit 1
