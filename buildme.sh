a84d35c68fd6395938c9fcf6935df897bda03358
Christian Daudt <christian.daudt@cypress.com>
2017-05-26 10:09:14 -0700

    Parse docker image hash from logs
    And produce an easy-to-grep version of it
    
    Change-Id: I844c6a1b98acb45a4f94fced775f2cd9c0909a27

--- buildme.sh
+++ buildme.sh
@@ -5,6 +5,8 @@ NOCACHE=${1:-true}
 COPYTO=${2:-}
 ADMINUSER=lava-admin
 PASSWORD=`cat /home/${ADMINUSER}/.pushpw`
+LOGS=$(mktemp /tmp/buildme.XXXXXX)
+
 
 function dockerpush()
 {
@@ -24,11 +26,14 @@ docker build \
   --label "build.status=`git status --short`" \
   --no-cache=${NOCACHE} \
   -t ${TAGNAME}:${VER} \
-  .
+  . \
+  | tee  ${LOGS}
 if [ $? -ne 0 ]
 then
   exit 1
 fi
+HASH=`grep "Successfully built " ${LOGS}|awk '{print $3}'`
+echo "IMAGE_BUILD:TAGNAME=${TAGNAME}:HASH=${HASH}"
 
 if [ "${COPYTO}A" != "A" ]
 then
